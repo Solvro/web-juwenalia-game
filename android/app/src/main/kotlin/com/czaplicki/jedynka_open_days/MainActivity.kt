8d13ffe42a2abb2de73ab530b22b40c70cfc6a7e
Antoni Czaplicki <antekczaplicki@gmail.com>
2026-04-24 16:50:52 +0200

    chore: change package name for the fourth time

--- android/app/src/main/kotlin/com/czaplicki/jedynka_open_days/MainActivity.kt
+++ android/app/src/main/kotlin/com/czaplicki/jedynka_open_days/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.solvro.juwenalia_app
+package pl.solvro.juwenalia_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -12,8 +11,6 @@ import GoogleMaps
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    // Google Maps must be configured before the plugin is registered.
-    GMSServices.provideAPIKey("AIzaSyDsf5hnTY2mk82eR6b3-hB7YqAIX-KkBVg")
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
